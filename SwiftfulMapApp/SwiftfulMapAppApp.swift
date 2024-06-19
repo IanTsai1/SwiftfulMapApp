02c2fd3d2bcf893699af6080ea3beee29c99f804
Ian Tsai <ian.tsai@gmail.com>
2024-06-19 18:32:35 -0400

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Ian Tsai on 2024/6/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
